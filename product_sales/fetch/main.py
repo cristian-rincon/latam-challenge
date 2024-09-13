18057246d863551f6e3063970d11d025492bba52
cristian-rincon <cristian.o.rincon.b@gmail.com>
2024-09-13 12:51:18 -0500

    refactor: Update Python code style and enhance CI workflow with linting and testing

--- product_sales/fetch/main.py
+++ product_sales/fetch/main.py
@@ -1,15 +1,17 @@
 import os
-from google.cloud import bigquery
+
 from flask import jsonify
+from google.cloud import bigquery
 
 # Definir variables de entorno
-PROJECT_ID = os.getenv('GCP_PROJECT')
-DATASET_ID = os.getenv('BIGQUERY_DATASET')
-TABLE_ID = os.getenv('BIGQUERY_TABLE')
+PROJECT_ID = os.getenv("GCP_PROJECT")
+DATASET_ID = os.getenv("BIGQUERY_DATASET")
+TABLE_ID = os.getenv("BIGQUERY_TABLE")
+
 
 def fetch_data(request):
     """Cloud Function para consultar datos desde BigQuery"""
-    
+
     # Inicializar cliente de BigQuery
     client = bigquery.Client()
 
@@ -26,21 +28,23 @@ def fetch_data(request):
     # Ejecutar la consulta
     try:
         query_job = client.query(query)  # Ejecutar la consulta
-        results = query_job.result()     # Obtener los resultados
+        results = query_job.result()  # Obtener los resultados
 
         # Convertir los resultados a un formato JSON
         data = []
         for row in results:
-            data.append({
-                "product_id": row.product_id,
-                "product_name": row.product_name,
-                "category": row.category,
-                "unit_price": row.unit_price,
-                "supplier": row.supplier
-            })
+            data.append(
+                {
+                    "product_id": row.product_id,
+                    "product_name": row.product_name,
+                    "category": row.category,
+                    "unit_price": row.unit_price,
+                    "supplier": row.supplier,
+                }
+            )
 
         return jsonify(data), 200
 
     except Exception as e:
-        print(f'Error al consultar BigQuery: {e}')
+        print(f"Error al consultar BigQuery: {e}")
         return jsonify({"error": str(e)}), 500
